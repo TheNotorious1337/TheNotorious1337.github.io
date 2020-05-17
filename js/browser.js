ebf62c9618c937ad350adb46765818aff232fa27
Adrian Jakubcik <cosxmidas67@gmail.com>
2020-05-17 20:35:18 +0200

    Javascript Hotfix

--- js/browser.js
+++ js/browser.js
@@ -2,7 +2,8 @@ console.log(navigator.userAgent);
 const body = document.querySelector('body');
 
 if(navigator.userAgent.indexOf('Firefox') == -1 && navigator.userAgent.indexOf('Edge') == -1) {
-    body.innerHTML = `<h1> Unsupported Browser! </h1>`
+    body.innerHTML = `<h1> Unsupported Browser! </h1>
+    <h1>For Now Please Use Either Microsoft Edge or Mozilla Firefox </h1>`
 }
 
 if(window.innerWidth < 780 || window.innerHeight < 780) {
