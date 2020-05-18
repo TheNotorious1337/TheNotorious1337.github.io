e36b5fb3073f8c71d43e3355eeb31a1c5d42e0fc
Adrian Jakubcik <cosxmidas67@gmail.com>
2020-05-18 13:32:09 +0200

    Added support for Opera

--- js/browser.js
+++ js/browser.js
@@ -1,15 +1,17 @@
 const body = document.querySelector('body');
 var ua = navigator.userAgent.toLowerCase();
+console.log(ua);
 
 if(ua.indexOf('firefox') == -1 && ua.indexOf('edge') == -1 && ua.indexOf('safari') == -1) {
     body.innerHTML = `<h1> Unsupported Browser! </h1>
     <h1>For Now Please Use Either Microsoft Edge or Mozilla Firefox or Safari </h1>`
 }
 
-if (ua.indexOf('chrome') > -1) {
+if (ua.indexOf('chrome') > -1 && ua.indexOf('opr') < 0) {
     body.innerHTML = `<h1> Unsupported Browser! </h1>
     <h1>For Now Please Use Either Microsoft Edge or Mozilla Firefox or Safari </h1>`
 } 
+
 
 // if(window.innerWidth < 780 || window.innerHeight < 780) {
 //     body.innerHTML = `<h1> Unsupported Browser Dimensions! </h1>`
