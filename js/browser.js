4b3410dfe48d5261e338eeaa3a82a33d960e1c84
Adrian Jakubcik <cosxmidas67@gmail.com>
2020-05-18 13:17:43 +0200

    Added Safari support

--- js/browser.js
+++ js/browser.js
@@ -1,10 +1,15 @@
-console.log(navigator.userAgent);
 const body = document.querySelector('body');
+var ua = navigator.userAgent.toLowerCase();
 
-if(navigator.userAgent.indexOf('Firefox') == -1 && navigator.userAgent.indexOf('Edge') == -1) {
+if(ua.indexOf('firefox') == -1 && ua.indexOf('edge') == -1 && ua.indexOf('safari') == -1) {
     body.innerHTML = `<h1> Unsupported Browser! </h1>
-    <h1>For Now Please Use Either Microsoft Edge or Mozilla Firefox </h1>`
+    <h1>For Now Please Use Either Microsoft Edge or Mozilla Firefox or Safari </h1>`
 }
+
+if (ua.indexOf('chrome') > -1) {
+    body.innerHTML = `<h1> Unsupported Browser! </h1>
+    <h1>For Now Please Use Either Microsoft Edge or Mozilla Firefox or Safari </h1>`
+} 
 
 // if(window.innerWidth < 780 || window.innerHeight < 780) {
 //     body.innerHTML = `<h1> Unsupported Browser Dimensions! </h1>`
