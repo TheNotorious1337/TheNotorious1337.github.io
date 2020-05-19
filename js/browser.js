e78177932b0963103b4cc62386f611a1a4fac7fa
Adrian Jakubcik <cosxmidas67@gmail.com>
2020-05-19 15:53:44 +0200

    ...

--- js/browser.js
+++ js/browser.js
@@ -4,12 +4,12 @@ console.log(ua);
 
 if(ua.indexOf('firefox') == -1 && ua.indexOf('edge') == -1 && ua.indexOf('safari') == -1) {
     body.innerHTML = `<h1> Unsupported Browser! </h1>
-    <h1>For Now Please Use Either Microsoft Edge or Mozilla Firefox or Safari or Opera</h1>`
+    <h1>For Now Please Use Either Microsoft Edg, Mozilla Firefox, Safari or Opera</h1>`
 }
 
 if (ua.indexOf('chrome') > -1 && ua.indexOf('opr') < 0) {
     body.innerHTML = `<h1> Unsupported Browser! </h1>
-    <h1>For Now Please Use Either Microsoft Edge or Mozilla Firefox or Safari or Opera</h1>`
+    <h1>For Now Please Use Either Microsoft Edge, Mozilla Firefox, Safari or Opera</h1>`
 } 
 
 
